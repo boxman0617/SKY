f3bbb1b64c25064309b0ca4ba4bfc8ea2eb9f3ac
Alan Tirado <atirado0617@gmail.com>
2012-02-15 17:31:53 -0800

    Changes to configure.php

--- configs/configure.php
+++ configs/configure.php
@@ -4,6 +4,7 @@ define('ENV', 'DEV');
 
 // Development Enviroment [DEV]
 $db_dev = array(
+    'MODEL_DRIVER' => 'MySQL',
     'DB_SERVER' => '10.10.0.6',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => '',
@@ -11,6 +12,7 @@ $db_dev = array(
 );
 // Testing Enviroment [TEST]
 $db_test = array(
+    'MODEL_DRIVER' => 'MySQL',
     'DB_SERVER' => '10.10.0.6',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => '',
@@ -18,6 +20,7 @@ $db_test = array(
 );
 // Production Enviroment [PRO]
 $db_pro = array(
+    'MODEL_DRIVER' => 'MySQL',
     'DB_SERVER' => '',
     'DB_USERNAME' => '',
     'DB_PASSWORD' => '',
@@ -31,6 +34,7 @@ switch (ENV)
         define('DB_USERNAME', $db_dev['DB_USERNAME']);
         define('DB_PASSWORD', $db_dev['DB_PASSWORD']);
         define('DB_DATABASE', $db_dev['DB_DATABASE']);
+        define('MODEL_DRIVER', $db_dev['MODEL_DRIVER']);
         
         define('ERROR_REPORT_LEVEL', E_ALL);
         define('ERROR_LOG_LEVEL', E_ALL);
@@ -42,6 +46,7 @@ switch (ENV)
         define('DB_USERNAME', $db_test['DB_USERNAME']);
         define('DB_PASSWORD', $db_test['DB_PASSWORD']);
         define('DB_DATABASE', $db_test['DB_DATABASE']);
+        define('MODEL_DRIVER', $db_test['MODEL_DRIVER']);
         
         define('ERROR_REPORT_LEVEL', E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_USER_NOTICE);
         define('ERROR_LOG_LEVEL', E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_USER_NOTICE);
@@ -53,6 +58,7 @@ switch (ENV)
         define('DB_USERNAME', $db_pro['DB_USERNAME']);
         define('DB_PASSWORD', $db_pro['DB_PASSWORD']);
         define('DB_DATABASE', $db_pro['DB_DATABASE']);
+        define('MODEL_DRIVER', $db_pro['MODEL_DRIVER']);
         
         define('ERROR_REPORT_LEVEL', E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_USER_NOTICE);
         define('ERROR_LOG_LEVEL', E_ALL ^ E_NOTICE ^ E_WARNING ^ E_STRICT);
